c59f13f3aa7cd502bda446320a417d3cded048bb
PabloCorcho <corchocaballeropablo@gmail.com>
2022-08-04 13:41:38 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py2dfdr',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GNU General Public License v3.0',
+    author='Pablo Corcho-Caballero',
+    author_email='pablo.corcho@uam.es',
+    description='2dfdr python wrapper for IFU (KOALA) data reduction'
+)
