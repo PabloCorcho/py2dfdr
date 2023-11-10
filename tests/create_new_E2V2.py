d0aa56ed20d18bc1135f812e6c567933a5a9f5b6
pcorchoc <p.corcho.caballero@rug.nl>
2023-11-10 11:39:54 +0100

    generalise test script for creating new detector mask

--- tests/create_new_E2V2.py
+++ tests/create_new_E2V2.py
@@ -5,21 +5,31 @@ in 2dfdr
 from astropy.io import fits
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
-path_to_2dfdr = '/home/pablo/Software/2dfdr_install'
-data = path_to_2dfdr + '/share/2dfdr/E2V2A.fits'
+#path_to_2dfdr = os.path.dirname(os.path.dirname(os.system("which aaorun")))
+#path_to_2dfdr = '/home/pablo/Software/2dfdr_install'
+path_to_2dfdr = "/Users/users/caballero/DATASERVER3/KOALA/2dfdr/2dfdr_install"
+print(f"2dfdr directory: {path_to_2dfdr}")
+
+data = os.path.join(path_to_2dfdr, 'share', '2dfdr', 'E2V2A.fits')
+print(f"Path to detector: {data}")
 
 hdul = fits.open(data)
 # Save the old version
 old_data = hdul[0].data.copy()
 #hdul.writeto(path_to_2dfdr + '/share/2dfdr/E2V2A_old.fits')
-old_data = fits.getdata(path_to_2dfdr + '/share/2dfdr/E2V2A_old.fits')
-
+original_file = data.replace(".fits", "_old.fits")
+if not os.path.isfile(original_file):
+    print(f"Create a backup of the old file: {original_file}")
+    hdul.writeto(original_file, overwrite=True)
+old_data = fits.getdata(original_file)
 
 
 # Create a new version
 hdul[0].data = np.zeros_like(old_data)
 hdul.writeto(data, overwrite=True)
+print(f"New detector data saved as {data}")
 
 plt.figure(figsize=(15, 5))
 plt.subplot(121)
